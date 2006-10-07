858b8be8a9ae51f781bad6088712024c739d2ad1
Michiel Scholten <michiel@aquariusoft.org>
2006-10-07 15:35:21 +0000

    - Changed the thumbnailing support of galleries to use the gallery description files - Moved image generating routines to the index.php - Added support for off-site images
    --HG--
    extra : convert_revision : svn%3Ac6461f93-7114-0410-893d-dbe9e6e34e10/trunk%4013

--- core/image.php
+++ core/image.php
@@ -1,21 +1,36 @@
 <?php
+
 include_once('config.php');
 include_once('modules/mod_toolkit.php');
 include_once('modules/mod_site.php');
+include_once('modules/mod_qik_files.php');
+include_once('modules/mod_tagsections.php');
+/*
+include_once('setup_qik_framework.php');
+*/
 
 error_reporting( E_ALL );
 
 $action = getRequestParam('action', null);
 $kind = getRequestParam('kind', null);
-$filename = getRequestParam('filename', null);
+//$filename = getRequestParam('filename', null);
 
 if ('makethumb' == $action)
 {
+	/*
 	if ('gallery' == $kind)
 	{
 		$destfile = str_replace('/', '_', $filename);
 		$filename = realpath(dirname(__FILE__)) . '/images/gallery/' . $filename;
 		$destfile = realpath(dirname(__FILE__)) . '/images/gallery/thumbs/'. $destfile;
+	}
+	*/
+	if ('gallery' == $kind)
+	{
+		$gallery = getRequestParam('gallery', null);
+		$file = getRequestParam('file', -1);
+		$galleryItems = getGallery($skel, $gallery . ':' . $file . ':1');
+		print_r($galleryItems);
 	}
 	header("Content-type: image/jpeg");
 	makeThumbnail($filename, 80, $destfile);
