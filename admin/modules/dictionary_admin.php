3177a1a50ee8b6e17edd9cedc27fff82601abccc
Michiel Scholten <michiel@aquariusoft.org>
2007-07-21 15:06:45 +0000

    2007-07-19: added session support
    --HG--
    extra : convert_revision : svn%3Ac6461f93-7114-0410-893d-dbe9e6e34e10/trunk%4025

--- admin/modules/dictionary_admin.php
+++ admin/modules/dictionary_admin.php
@@ -7,6 +7,8 @@ if ($skel['language'] == 'nl')
 {
 	$skel['dict']['admin_welcome'] = 'Welkom op de Admin pagina. Hier kunt u teksten etc aanpassen.';
 	$skel['dict']['admin_home'] = 'Admin - overzicht';
+	$skel['dict']['admin_loggedout'] = 'Uitgelogged';
+	$skel['dict']['admin_backtologin'] = 'Go back to the login page';
 
 	//$skel['dict'][''] = '';
 } else
@@ -14,6 +16,8 @@ if ($skel['language'] == 'nl')
 	/* default to en */
 	$skel['dict']['admin_welcome'] = 'Welcome to the Admin page. Here you can edit texts etc.';
 	$skel['dict']['admin_home'] = 'Admin - overview';
+	$skel['dict']['admin_loggedout'] = 'Logged out';
+	$skel['dict']['admin_backtologin'] = 'Go back to the login page';
 
 	//$skel['dict'][''] = '';
 	//$skel['dict']['gallerykey_x_not_found'] = 'ERROR: gallery "%s" not found';
