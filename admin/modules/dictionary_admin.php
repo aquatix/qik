f7eb5fdab528e3102a480fd438084664a157b535
Michiel Scholten <michiel@aquariusoft.org>
2007-08-05 15:06:40 +0000

    Implemented a lot of the config [page overview, page editing - not saving yet, login etc]
    --HG--
    extra : convert_revision : svn%3Ac6461f93-7114-0410-893d-dbe9e6e34e10/trunk%4028

--- admin/modules/dictionary_admin.php
+++ admin/modules/dictionary_admin.php
@@ -5,19 +5,35 @@
 
 if ($skel['language'] == 'nl')
 {
-	$skel['dict']['admin_welcome'] = 'Welkom op de Admin pagina. Hier kunt u teksten etc aanpassen.';
+	$skel['dict']['admin_welcome'] = 'Welkom op de Admin pagina. Log in om door te gaan.';
 	$skel['dict']['admin_home'] = 'Admin - overzicht';
-	$skel['dict']['admin_loggedout'] = 'Uitgelogged';
-	$skel['dict']['admin_backtologin'] = 'Go back to the login page';
+	$skel['dict']['admin_login'] = 'Admin - log in';
+	$skel['dict']['admin_editpage'] = 'Admin - wijzig pagina';
+	$skel['dict']['admin_editpage_explanation'] = 'Kies een pagina om er de teksten van aan te passen:';
+	$skel['dict']['admin_editingpage'] = 'Bezig met wijzigen van pagina';
+	$skel['dict']['admin_loggedout'] = 'Admin - Uitgelogged';
+	$skel['dict']['admin_backtologin'] = 'Ga terug naar de login page';
+	$skel['dict']['admin_back2overview'] = 'Ga terug naar het overzicht';
+	$skel['dict']['admin_username'] = 'gebruikersnaam';
+	$skel['dict']['admin_password'] = 'wachtwoord';
+	$skel['dict']['admin_save'] = 'opslaan';
 
 	//$skel['dict'][''] = '';
 } else
 {
 	/* default to en */
-	$skel['dict']['admin_welcome'] = 'Welcome to the Admin page. Here you can edit texts etc.';
+	$skel['dict']['admin_welcome'] = 'Welcome to the Admin page. Please log in to continue.';
 	$skel['dict']['admin_home'] = 'Admin - overview';
+	$skel['dict']['admin_login'] = 'Admin - log in';
+	$skel['dict']['admin_editpage'] = 'Admin - edit page';
+	$skel['dict']['admin_editpage_explanation'] = 'Choose a page to edit the text off:';
+	$skel['dict']['admin_editingpage'] = 'Editing page';
 	$skel['dict']['admin_loggedout'] = 'Logged out';
 	$skel['dict']['admin_backtologin'] = 'Go back to the login page';
+	$skel['dict']['admin_back2overview'] = 'Go back to the overview page';
+	$skel['dict']['admin_username'] = 'username';
+	$skel['dict']['admin_password'] = 'password';
+	$skel['dict']['admin_save'] = 'save';
 
 	//$skel['dict'][''] = '';
 	//$skel['dict']['gallerykey_x_not_found'] = 'ERROR: gallery "%s" not found';
