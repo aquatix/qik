bf2a6cbaebd499a8d119978b92dfecc1a4c31a10
Michiel Scholten <michiel@aquariusoft.org>
2007-09-10 14:27:44 +0000

    - Revamped the default style of the example sites [both single language and multilingual] - Merged struct.css into style.css: use only one stylesheet anymore - Fleshed out the default content a bit - Some other fixes [mostly to the pagetemplate.php's]
    --HG--
    extra : convert_revision : svn%3Ac6461f93-7114-0410-893d-dbe9e6e34e10/trunk%4039

--- example_multilingual/config.php
+++ example_multilingual/config.php
@@ -1,26 +1,33 @@
 <?php
+/**
+ * Config file for Qik site framework
+ * Example site, single language
+ */
 
-	/*
-	 * set the language to be used by the framework
-	 * you can choose between en for English and nl for Dutch
-	 */
-	$skel['language'] = 'en';
-	$skel['multilanguage'] = true;
+/*
+ * set the language to be used by the framework
+ * you can choose between en for English and nl for Dutch
+ */
+$skel['language'] = 'en';
+$skel['multilanguage'] = true;
 
-	/* Additional information to be shown when a user gets a 'page not found' */
-	$skel['404_message'] = '<p>If you are looking for the overload feedreader, please <a href="https://overload.aquariusoft.org">look at overloads new address</a></p>';
-	//$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
+/* Additional information to be shown when a user gets a 'page not found' */
+$skel['404_message'] = '<p>If you are looking for the overload feedreader, please <a href="https://overload.aquariusoft.org">look at overloads new address</a></p>';
+//$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
 
-	/* Set usedb to false for using flat files [only supported mechanism at the moment] */
-	$skel['usedb'] = false;
-	$skel['dbhost'] = '';
-	$skel['dbname'] = '';
-	$skel['dbuser'] = '';
-	$skel['dbpass'] = '';
+/* Set max thumbnail size [optional, defaults to 80px] */
+$skel['thumbsize'] = 100;
 
-	$skel['logfile'] = '/var/log/qik/aquariusoftorg.log';
-	$skel['logpass'] = 'password';
+/* Set usedb to false for using flat files [only supported mechanism at the moment] */
+$skel['usedb'] = false;
+$skel['dbhost'] = '';
+$skel['dbname'] = '';
+$skel['dbuser'] = '';
+$skel['dbpass'] = '';
 
-	/* the text to be displayed in the footer */
-	$skel['copyright'] = '&copy; 1999-2006 webmaster (at) aquariusoft.org';
+$skel['logfile'] = '/var/log/qik/aquariusoftorg.log';
+$skel['logpass'] = 'password';
+
+/* the text to be displayed in the footer */
+$skel['copyright'] = '&copy; 1999-2006 webmaster (at) aquariusoft.org';
 ?>
