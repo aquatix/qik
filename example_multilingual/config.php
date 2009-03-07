9fef9ba05b87cb9f7860fed8455154903f8d89e0
mbscholt <none@none>
2009-03-07 10:08:35 +0000

    Made the title of the site configurable, added some functions to the example sites and translated the picture page of the gallery
    --HG--
    extra : convert_revision : svn%3Ac6461f93-7114-0410-893d-dbe9e6e34e10/trunk%4069

--- example_multilingual/config.php
+++ example_multilingual/config.php
@@ -11,6 +11,12 @@
 $skel['language'] = 'en';
 $skel['multilanguage'] = true;
 
+/* Enable or disable Qik's debug mode */
+$skel['debugmode'] = false;
+
+/* The title of the site, shown in the browser's titlebar */
+$skel['sitetitle'] = 'Qik example site (multilingual)';
+
 /* Additional information to be shown when a user gets a 'page not found' */
 $skel['404_message'] = '<p>If you are looking for the overload feedreader, please <a href="https://overload.aquariusoft.org">look at overloads new address</a></p>';
 //$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
