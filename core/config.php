3ecc4199a3af908d8b49ca8c84f6374a9822a440
Michiel Scholten <michiel@aquariusoft.org>
2009-03-07 10:08:35 +0000

    Made the title of the site configurable, added some functions to the example sites and translated the picture page of the gallery
    --HG--
    extra : convert_revision : svn%3Ac6461f93-7114-0410-893d-dbe9e6e34e10/trunk%4069

--- core/config.php
+++ core/config.php
@@ -11,9 +11,15 @@
 $skel['language'] = 'en';
 $skel['multilanguage'] = false;
 
+/* Enable or disable Qik's debug mode */
+$skel['debugmode'] = false;
+
+/* The title of the site, shown in the browser's titlebar */
+$skel['sitetitle'] = 'Qik example site (single language)';
+
 /* Additional information to be shown when a user gets a 'page not found' */
-$skel['404_message'] = '<p>If you are looking for the overload feedreader, please <a href="https://overload.aquariusoft.org">look at overloads new address</a></p>';
-//$skel['404_message'] .= '<p>Also, the bugtracker, gallery, forum and webmail and the weblog dammIT will be online again soon.</p>';
+$skel['404_message'] = '<p>For more information about Qik, see <a href="http://aquariusoft.org/page/html/qik/">its page on aquariusoft.org</a></p>';
+//$skel['404_message'] .= '<p>Another message.</p>';
 
 /* Set max thumbnail size [optional, defaults to 80px] */
 $skel['thumbsize'] = 100;
@@ -25,9 +31,9 @@ $skel['dbname'] = '';
 $skel['dbuser'] = '';
 $skel['dbpass'] = '';
 
-$skel['logfile'] = '/var/log/qik/aquariusoftorg.log';
+$skel['logfile'] = '/var/log/qik/yoursite.log';
 $skel['logpass'] = 'password';
 
 /* the text to be displayed in the footer */
-$skel['copyright'] = '&copy; 1999-2007 webmaster (at) aquariusoft.org';
+$skel['copyright'] = '&copy; 2009 webmaster (at) example.com';
 ?>
--- example_multilingual/site/en/pagetemplate.php
+++ example_multilingual/site/en/pagetemplate.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Page template for Qik site framework, single language
- * v0.2.01 2007-09-10
+ * Page template for Qik site framework, multiple languages
+ * $Id$
  */
 
 /*
@@ -14,8 +14,7 @@ function buildPage($skel, $page_title, $navbar, $subnavbar, $body)
 	$template .= "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
 	//$template .= "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">\n";
 	$template .= "<head>\n";
-	$template .= "<title>aquariusoft.org | " . $page_title . "</title>\n";
-	//$template .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . $skel["base_uri"] . "css/struct.css\"/>\n";
+	$template .= "<title>" . $page_title . ' | ' . $skel['sitetitle'] . "</title>\n";
 	$template .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . $skel["base_uri"] . "css/style.css\"/>\n";
 	$template .= "</head>\n<body>\n";
 
@@ -125,5 +124,103 @@ function buildSubnav($skel, $section, $subsections)
 	$result .= "\t\t\t<li><a href=\"http://aquariusoft.org/page/html/qik/\">build with qik</a></li>\n";
 	$result .= "\t\t</ul>\n";
 	return $result . "\t</div>\n";
+}
+
+
+/*
+ * If site has an admin section, this builds its subnavigation
+ */
+function buildAdminSubnav($skel, $section, $subsections)
+{
+	$result = "\t<div class=\"subnavbar\">\n";
+	$result .= "\t<h2>" . $skel['sectionname'] . "</h2>\n";
+	for ($i = 0; $i < count($subsections); $i++)
+	{
+		if ('' != trim($subsections[$i]))
+		{
+			$pagekey = getKey($subsections[$i]);
+			$page = getValue($subsections[$i]);
+			if ('#' != $pagekey[0] && '' != trim($page))	// '#' denotes a comment in the description file
+			{
+				if (isset($skel['page']) && $pagekey == $skel['page'])
+				{
+					$active = ' class="highlight"';
+				} else
+				{
+					$active = '';
+				}
+				$result .= "<a href=\"" . $skel['base_uri'] . 'admin/' . $section . '/' . $pagekey . '/"' . $active . '>' . trim($page) . "</a> | ";
+			}
+		}
+	}
+	return $result . "</div>\n";
+}
+
+
+/*
+ * Build a news item (from @@@news@@@ tag) out of $title and $content
+ */
+function buildNewsItem($skel, $title, $content)
+{
+	$result .= "<div class=\"news\">\n";
+	$result .= "\t<div class=\"date\">" . $title . "</div>";
+	$result .= "<div class=\"newscontent\">" . $content . "</div>\n</div>\n\n";
+	return $result;
+}
+
+
+/*
+ * Build the gallery from the items provided in $galleryitems, which are of the form
+ * $galleryitems[$i]['title']
+ * $galleryitems[$i]['filename']
+ * $galleryitems[$i]['targetfilename']
+ */
+function buildGallery($skel, $galleryname, $galleryitems)
+{
+	$result = '';
+	$imagecounter = 0;
+	for ($i = 0; $i < count($galleryitems); $i++)
+	{
+		$imagecounter++;
+		//$result .= "<div class=\"galleryimage\"><div class=\"theimage\"><a href=\"" . $galleryitems[$i]['targetfilename'] . "\" target=\"_blank\"><img src=\"" . $galleryitems[$i]['filename'] . "\" alt=\"" . $galleryitems[$i]['title'] . "\" /></a></div><p>" . $galleryitems[$i]['title'] . "</p>\n</div>\n";
+		$result .= "<div class=\"galleryimage\"><div class=\"theimage\"><a href=\"" . $galleryitems[$i]['galleryitem'] . "\" target=\"_blank\"><img src=\"" . $galleryitems[$i]['filename'] . "\" alt=\"" . $galleryitems[$i]['title'] . "\" /></a></div><p>" . $galleryitems[$i]['title'] . "</p>\n</div>\n";
+		/* Insert a horizontal ruler every 4 lines, so unevenly sized items show up correctly */
+		/*if ($imagecounter % 4 == 0)
+		{
+			$result .= "\n<hr />\n";
+		}*/
+	}
+	return $result;
+}
+
+
+/*
+ * Build the page in a gallery showing the previous/next thumbnails and the current picture
+ */
+function buildGalleryPage($skel, $galleryname, $galleryitems, $item)
+{
+	$item = $item - 1; /* Arrays are 0-based */
+        $base = $skel['base_server'] . $skel['base_uri'];
+
+	$body  = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
+	$body .= "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n";
+	$body .= "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n";
+	$body .= "<head>\n";
+	//$body .= "<title>" . $page_title . " | Mapleleaves Heemskerk</title>\n";
+	$body .= '<title>' . dict($skel, 'picture') . ': ' . $galleryitems[$item]['title'] . ' | ' . $skel['sitetitle'] . "</title>\n";
+	$body .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"" . $skel["base_uri"] . "css/style.css\"/>\n";
+        $body .= "<table class=\"gallerynav\" width=\"100%\">\n";
+	$previtem = max(0, $item-1);
+	$nextitem = min(count($galleryitems)-1, $item+1);
+        $body .= '<tr><td height="' . ($skel['thumbsize']+5) . '" width="' . ($skel['thumbsize']+5) . '" style="background-image: url(' . $base . $galleryitems[0]['filename'] . ');"><a href="' . $galleryitems[0]['galleryitem'] . '">|&lt;</a></td><td width="' . ($skel['thumbsize']+5) . '" style="background-image: url(' . $base . $galleryitems[$previtem]['filename'] . ');"><a href="' . $galleryitems[$previtem]['galleryitem'] . '">&lt;&lt;</a></td>';
+        //$body .= '<td>Gallery</td>';
+        $body .= '<td>' . $galleryitems[$item]['title'] . '</td>';
+        $body .= '<td width="' . ($skel['thumbsize']+5) . '" style="background-image: url(' . $base . $galleryitems[$nextitem]['filename'] . ');"><a href="' . $galleryitems[$nextitem]['galleryitem'] . '">&gt;&gt;</a></td><td width="' . ($skel['thumbsize']+5) . '" style="background-image: url(' . $base . $galleryitems[count($galleryitems)-1]['filename'] . ');"><a href="' . $galleryitems[count($galleryitems)-1]['galleryitem'] . '">&gt;|</a></td></tr>';
+	$body .= "\n<tr><td colspan=\"5\">";
+        $body .= '<img src="' . $galleryitems[$item]['targetfilename'] . '" />';
+        $body .= "</td></tr>\n</table>\n";
+	$body .= "</body></html>\n";
+
+	return $body;
 }
 ?>
